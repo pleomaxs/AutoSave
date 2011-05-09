379ea09698a061071351f30893496f7239d8f6ef
pleomaxs <fsmaloy@gmail.com>
2011-05-09 05:36:06 -0700

    Edited sh/cereal/bukkit/plugin/AutoSave/AutoSaveConfig.java via GitHub

--- sh/cereal/bukkit/plugin/AutoSave/AutoSaveConfig.java
+++ sh/cereal/bukkit/plugin/AutoSave/AutoSaveConfig.java
@@ -24,7 +24,7 @@ import java.util.UUID;
 public class AutoSaveConfig {
 
 	// Messages
-	public String messageBroadcastPre = "World Auto-Saving";
+	public String messageBroadcastPre = "Авто-Сохранение";
 	public String messageBroadcastPost = "World Auto-Save Complete";
 	public String messageStatusFail = "Auto Save has stopped, check the server logs for more info";
 	public String messageStatusNotRun = "Auto Save is running but has not yet saved.";
